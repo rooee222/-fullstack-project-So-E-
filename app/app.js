eb3ff6b56276ef2e736654fd7c267b24fe3f223c
maroona1997@gmail.com <you@example.com>
2026-03-03 16:54:59 +0000

    dynamic route ex 4

--- app/app.js
+++ app/app.js
@@ -29,8 +29,20 @@ app.get("/user/:id", function(req, res) {
 
 // creating dynamic route which where a user may request /student/:name/:id
 app.get("/student/:name/:id", function(req, res) {
-    res.send("Name: " + req.params.name + " ID: " + req.params.id);
+    res.send(`
+        <table border="1">
+            <tr>
+                <th>Name</th>
+                <th>ID</th>
+            </tr>
+            <tr>
+                <td>${req.params.name}</td>
+                <td>${req.params.id}</td>
+            </tr>
+        </table>
+    `);
 });
+
 
 // Create a route for testing the db
 app.get("/db_test", function(req, res) {
